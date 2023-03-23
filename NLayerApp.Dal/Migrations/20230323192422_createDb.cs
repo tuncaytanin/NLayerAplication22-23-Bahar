2dc6f53dbff2ff2de189ebe89e2ad5057ae465e1
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2023-03-23 22:30:35 +0300

    Migration komple silindi ve yeniden oluşturuldu

---
+++ NLayerApp.Dal/Migrations/20230323192422_createDb.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NLayerApp.Dal.Migrations
+{
+    public partial class createDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()"),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreateDate", "Description", "IsDeleted", "Name", "UpdateDate" },
+                values: new object[] { 1, new DateTime(2023, 3, 23, 22, 24, 22, 725, DateTimeKind.Local).AddTicks(2885), "Elektornik Eşyalar", false, "Elektronik Eşyalar", null });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreateDate", "Description", "IsDeleted", "Name", "UpdateDate" },
+                values: new object[] { 2, new DateTime(2023, 3, 23, 22, 24, 22, 725, DateTimeKind.Local).AddTicks(2892), "Çantalar", false, "Çantalar", null });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreateDate", "Description", "IsDeleted", "Name", "UpdateDate" },
+                values: new object[] { 3, new DateTime(2023, 3, 23, 22, 24, 22, 725, DateTimeKind.Local).AddTicks(2893), "Telefonlar", false, "Telefonlar", null });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Code", "CreateDate", "Name", "Price", "Stock", "UpdateDate" },
+                values: new object[] { 1, 1, "0001", new DateTime(2023, 3, 23, 22, 24, 22, 725, DateTimeKind.Local).AddTicks(3062), "Tost Makinası", 1500m, 15, null });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Code", "CreateDate", "Name", "Price", "Stock", "UpdateDate" },
+                values: new object[] { 2, 2, "00002", new DateTime(2023, 3, 23, 22, 24, 22, 725, DateTimeKind.Local).AddTicks(3064), "Leptop Çantası", 750m, 20, null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
