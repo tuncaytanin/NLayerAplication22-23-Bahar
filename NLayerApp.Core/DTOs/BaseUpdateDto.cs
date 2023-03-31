77bfd8fdc313a96c1788be311f197d1181214f60
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2023-03-31 18:05:58 +0300

    Dto Mantığı eklendi. PRoduct Dto için ilk Add ve Update oluşturuldu

---
+++ NLayerApp.Core/DTOs/BaseUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.DTOs
+{
+    public class BaseUpdateDto
+    {
+        public int Id { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
